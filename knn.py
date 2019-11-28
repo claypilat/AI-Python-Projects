2aad7778511c34e5101d1fb7fa631f5017b9c4c8
claypilat <44934339+claypilat@users.noreply.github.com>
2019-11-28 16:21:20 +1030

    Update knn.py

--- knn.py
+++ knn.py
@@ -11,13 +11,13 @@ data = data = pd.read_csv("C:/Users/cpilat/Desktop/Projects/Python/whatever/AI-P
 print(data.head())
 
 le = preprocessing.LabelEncoder()
-buying = le.fit_transform(list(data["buying"])
-maint = le.fit_transform(list(data["maint"])
-door = le.fit_transform(list(data["door"])
-persons = le.fit_transform(list(data["persons"])
-lug_boot = le.fit_transform(list(data["lug_boot"])
-safety = le.fit_transform(list(data["safety"])
-cls = le.fit_transform(list(data["class"])
+buying = le.fit_transform(list(data["buying"]))
+maint = le.fit_transform(list(data["maint"]))
+door = le.fit_transform(list(data["door"]))
+persons = le.fit_transform(list(data["persons"]))
+lug_boot = le.fit_transform(list(data["lug_boot"]))
+safety = le.fit_transform(list(data["safety"]))
+cls = le.fit_transform(list(data["class"]))
 
 
 predict = "class"
@@ -26,4 +26,22 @@ predict = "class"
 X = list(zip(buying, maint, door, persons, lug_boot, safety))
 y = list(cls)
 
-X_train, X_test, y_train, y_split = sklearn.model_selection.train_test_split(X, y,test_size=0.1)
+X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y,test_size=0.1)
+
+model = KNeighborsClassifier(n_neighbors=5)
+
+model.fit(X_train, y_train)
+acc = model.score(X_test, y_test)
+
+
+predicted = model.predict(X_test)
+
+print(acc)
+
+names = ["unacc", "acc", "good", "vgood"]
+
+for x in range(len(predicted)):
+    print("Predicted: ", names[predicted[x]], "Data: ", X_test[x], "Actual: ", names[y_test[x]])
+    n = model.kneighbors([X_test[x]], 9, True)
+    print("N: ", n)
+
