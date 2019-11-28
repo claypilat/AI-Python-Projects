d83d1d312bc894e76f79ab1ea1c4c9c5781a142b
claypilat <44934339+claypilat@users.noreply.github.com>
2019-11-28 11:44:36 +1030

    Changes

--- knn.py
+++ knn.py
@@ -6,6 +6,24 @@ import numpy as np
 from sklearn import  linear_model, preprocessing
 
 
-data = data = pd.read_csv("C:/Users/cpilat/Desktop/Projects/Python/whatever/car.data", sep=',')
+data = data = pd.read_csv("C:/Users/cpilat/Desktop/Projects/Python/whatever/AI-Python-Projects/car.data", sep=',')
 
 print(data.head())
+
+le = preprocessing.LabelEncoder()
+buying = le.fit_transform(list(data["buying"])
+maint = le.fit_transform(list(data["maint"])
+door = le.fit_transform(list(data["door"])
+persons = le.fit_transform(list(data["persons"])
+lug_boot = le.fit_transform(list(data["lug_boot"])
+safety = le.fit_transform(list(data["safety"])
+cls = le.fit_transform(list(data["class"])
+
+
+predict = "class"
+
+
+X = list(zip(buying, maint, door, persons, lug_boot, safety))
+y = list(cls)
+
+X_train, X_test, y_train, y_split = sklearn.model_selection.train_test_split(X, y,test_size=0.1)
