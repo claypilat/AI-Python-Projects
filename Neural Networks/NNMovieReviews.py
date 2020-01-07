0da1fb42904edeab2eeeb8f41b36c4d7fd4b601d
claypilat <44934339+claypilat@users.noreply.github.com>
2020-01-07 13:37:56 +1030

    Create NNMovieReviews.py

---
+++ Neural Networks/NNMovieReviews.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = keras.datasets.imdb
+
+(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words = 10000)
+
+print(train_data[0])
