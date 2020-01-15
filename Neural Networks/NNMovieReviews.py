15db0419f69b530bbc8512050f39cd2a77a003e2
claypilat <44934339+claypilat@users.noreply.github.com>
2020-01-15 14:02:29 +1030

    Update NNMovieReviews.py

--- Neural Networks/NNMovieReviews.py
+++ Neural Networks/NNMovieReviews.py
@@ -8,3 +8,22 @@ data = keras.datasets.imdb
 (train_data, train_labels), (test_data, test_labels) = data.load_data(num_words = 10000)
 
 print(train_data[0])
+
+#grabs the tuples
+word_index = data.get_word_index()
+
+#k for key, v for value
+word_index = {k:(v+3) for k, v in word_index.items()}
+word_index["<PAD>"] = 0
+word_index["<START>"] = 1
+word_index["<UNK>"] = 2
+word_index["<UNUSED>"] = 3
+
+#swap all values and keys
+reverse_word_index = dict([(value,key) for (key, value) in word_index.items()])
+
+
+def decode_review(text):
+  return " ".join([reverse_word_index.get(i, "?") for i in text])
+
+print(decode_review(test_data[0]))
